b45c579205f54b8537ad025fead66d1b1d51871a
Kirill Kniazev <kirill.kniazev@divio.ch>
2016-07-27 15:35:34 +0300

    Album creator: models and simple admin

--- album_creator/admin.py
+++ album_creator/admin.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album, Image, AlbumImageRelation
+
+
+class AlbumImageInline(admin.StackedInline):
+    model = AlbumImageRelation
+    extra = 1
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'images_count', )
+    inlines = (AlbumImageInline, )
+
+    def images_count(self, obj):
+        return obj.images.count()
+    images_count.short_description = 'Images count'
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    inlines = (AlbumImageInline, )
+    fields = ('image_file', 'image_url', )
--- album_creator/models.py
+++ album_creator/models.py
@@ -1,5 +1,65 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible, force_text
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Album(models.Model):
+    name = models.CharField(
+        verbose_name='Album name',
+        max_length=140,
+    )
+
+    def __str__(self):
+        return force_text(self.name)
+
+
+@python_2_unicode_compatible
+class Image(models.Model):
+    """
+    Image that is fetched from tweet for a specific hash tag (album).
+    Keeps track of the original image url to awoid duplicates.
+    """
+    album = models.ManyToManyField(
+        to=Album,
+        related_name='images',
+        through='AlbumImageRelation',
+    )
+    image_file = models.ImageField(
+        verbose_name='Image',
+        help_text='Fetched image file',
+        upload_to='uploads/',
+    )
+    image_url = models.URLField(
+        verbose_name='Original image url',
+        unique=True,
+    )
+
+    def __str__(self):
+        return force_text(self.image_url)
+
+
+@python_2_unicode_compatible
+class AlbumImageRelation(models.Model):
+    """
+    Relation between Album and Image, since one image can belong to multiple albums.
+    Keeps track of the tweet and the status of notification.
+    """
+    album = models.ForeignKey(
+        to='Album',
+    )
+    image = models.ForeignKey(
+        to='Image',
+    )
+    tweet_url = models.URLField(
+        verbose_name='Tweet url',
+        help_text='Tweet url were the image appeared',
+    )
+    notified = models.BooleanField(
+        default=False,
+    )
+
+    def __str__(self):
+        return force_text(self.pk)
